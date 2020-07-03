358c92cbbaae253a92c0f6fb67c0c9260eedda97
Alex <munroa.alex@gmail.com>
2020-07-03 16:52:01 +0800

    Update asx_analysis.py

--- asx_analysis.py
+++ asx_analysis.py
@@ -165,3 +165,25 @@ def check_criteria():
 
 
 check_criteria()
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv("stock_dfs/{}.csv".format(ticker))
+        df.set_index("Date", inplace=True)
+        df.rename(columns={"Adj Close": ticker}, inplace=True)
+        df.drop(["Open", "High", "Low", "Close", "Volume"], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how="outer")
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv("sp500_joined_closes.csv")
